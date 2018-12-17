b0b3dda58d721f50733179c1069f6c47aceb88c6
Dylan Tracey <Ddylskid@aol.com>
2018-12-17 03:55:25 +0000

    Remove debugging

--- utils/redis_cache.py
+++ utils/redis_cache.py
@@ -2,22 +2,10 @@ from settings import r
 
 
 def redis_register_split(index, ip, cookie_id, site):
-    print("set")
-    print("-----")
-    print(ip)
-    print("-----")
-    print(site)
-    sys.stdout.flush()
     r.set(index + '-' + ip + '-' + site, cookie_id, ex=10)
 
 
 def redis_retrieve_split(index, ip, site):
-    print("get")
-    print("-----")
-    print(ip)
-    print("-----")
-    print(site)
-    sys.stdout.flush()
     return r.get(index + '-' + ip + '-' + site)
 
 
