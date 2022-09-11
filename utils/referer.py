4cbf6a9137065414723c506faef3d1a0ebc63a22
DylanTracey <Ddylskid@aol.com>
2022-09-11 11:39:54 +0100

    debug url checker

--- utils/referer.py
+++ utils/referer.py
@@ -7,4 +7,5 @@ def check_referer(request, urls):
     simulate different web servers based on the base domains. Aborts if site won't exist for this base domain.
     """
     if not request.url_root in urls:
+        print(request.url_root)
         abort(404)
